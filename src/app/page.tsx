a8669fa2761fb47171e8bea7ed3a89b350394aee
benitdev <neihtben@gmail.com>
2023-06-15 22:24:23 +0700

    done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,112 +1,99 @@
-import Image from 'next/image'
+"use client"
+
+import CongratulationParticle from "@/components/CongratulationParticles"
+import { useRef, useState } from "react"
+
+const colors = [
+  "rgb(190 24 93)",
+  "#6a5af9",
+  "#d61efd",
+  "rgb(220, 38, 38)",
+  "rgb(157 23 77)",
+  "rgb(161 98 7)",
+  "black",
+]
+
+function getRandomColor() {
+  const index = Math.floor(Math.random() * colors.length)
+  return colors[index]
+}
 
 export default function Home() {
+  const [number, setNumber] = useState<number>(0)
+  const [numberResult, setNumberResult] = useState<number>(0)
+  const [isRunning, setIsRunning] = useState(false)
+  const intervalRef = useRef<NodeJS.Timer | null>(null)
+
+  function handleStartClick() {
+    setIsRunning(true)
+    const intervalId = setInterval(() => {
+      setNumber(Math.ceil(Math.random() * 300))
+    }, 100)
+    intervalRef.current = intervalId
+    setTimeout(() => {
+      setIsRunning(false)
+      setNumberResult(Math.ceil(Math.random() * 300))
+      clearTimeout(intervalId)
+    }, 5000)
+  }
+  const color = getRandomColor()
+  console.log(numberResult)
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <main className="flex h-screen flex-col items-center justify-between p-24 overflow-hidden">
+      <div className="flex flex-col items-center gap-4">
+        <h1 className="text-[100px] tracking-widest font-extrabold uppercase text-slate-200 leading-none">
+          Kozo<span className="text-gray-700 ">co</span>m
+        </h1>
+        <h2 className="text-slate-900 tracking-[20px] font-bold text-5xl uppercase animate-charcter">
+          bốc thăm may mắn
+        </h2>
+        <div className="relative rounded-full h-[550px] w-[550px] border-4 border-slate-900 flex items-center justify-center my-10">
+          <div className="absolute rounded-full h-[500px] w-[500px] border-4 border-slate-900 bg-black/50"></div>
+          <h1
+            style={{
+              color,
+            }}
+            className="absolute font-bold text-[150px] animate-fade"
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            {number}
+          </h1>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        {isRunning ? (
+          <span className="uppercase font-bold text-5xl text-slate-900">
+            Umbla xì bùa ...
+          </span>
+        ) : (
+          <button
+            className="px-10 py-5 rounded-2xl uppercase font-bold text-xl bg-red-700"
+            onClick={handleStartClick}
+          >
+            Con số gì đây
+          </button>
+        )}
+        {numberResult ? (
+          <div className="fixed z-50 inset-0 bg-black/70 backdrop-blur-sm flex items-center justify-center">
+            <div className="w-[400px] h-[400px] rounded-xl bg-cyan-700/70 p-5 backdrop-blur-sm">
+              <h1 className="uppercase font-bold text-2xl text-slate-900 text-center">
+                Xin chúc mừng!
+              </h1>
+              <h2 className="font-bold text-2xl text-slate-900 text-center mt-24">
+                <span className="text-[100px] text-red-600">
+                  {numberResult}
+                </span>
+              </h2>
+              <button
+                className="px-5 py-2 rounded-2xl uppercase font-bold text-xl bg-red-700 block mx-auto mt-20"
+                onClick={() => {
+                  setNumberResult(0)
+                  setNumber(0)
+                }}
+              >
+                Tiếp tục
+              </button>
+            </div>
+            <CongratulationParticle />
+          </div>
+        ) : null}
       </div>
     </main>
   )
